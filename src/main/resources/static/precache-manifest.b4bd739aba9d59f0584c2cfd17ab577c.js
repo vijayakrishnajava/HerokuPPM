caac47c57494863a7ffb777519ebe57d41da8afc
vijayakrishnajava <vijayakrishnajava@gmail.com>
2018-12-30 22:58:56 -0800

    last

---
+++ src/main/resources/static/precache-manifest.b4bd739aba9d59f0584c2cfd17ab577c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d9706920bdb49ef0a87",
+    "url": "/static/js/main.4d970692.chunk.js"
+  },
+  {
+    "revision": "7de37a6c646b70fa4140",
+    "url": "/static/js/1.7de37a6c.chunk.js"
+  },
+  {
+    "revision": "ae479fd0bdc3c0d5d5e4687660f00d8d",
+    "url": "/index.html"
+  }
+];
